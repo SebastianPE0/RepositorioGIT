f41160a16a2568ce59a6dac706f645cd1350fede
SEBPE <SEBPE@LAPTOP-R1LISP9E>
2023-12-26 22:57:12 -0500

    Agregar archivos de proyecto.

---
+++ RepositorioGIT/Program.cs
@@ -0,0 +1,11 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine("--------------------------------");
+        Console.WriteLine("UNIVERSIDAD CENTRAL DEL ECUADOR\n" +
+            "ANTONY SEBASTIÁN PÉREZ GAONA\n" +
+            "MARCOS DE DESARROLLO II");
+    }
+}
